fd80c5873416191d8d286019856f7fbacea5e73a
TJ Sornet <tsornet@cisco.com>
2020-07-23 19:46:22 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./components/GlobalStyles";
+import { lightTheme, darkTheme } from "./components/Themes";
+import "./App.css";
 
 function App() {
+  const [theme, setTheme] = React.useState("light");
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <>
+        <GlobalStyles />
+        <div className="App">
+          <button className="App-button" onClick={themeToggler}>Switch Theme</button>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Themes.js
@@ -0,0 +1,16 @@
+export const lightTheme = {
+  body: "#FFF !important",
+  text: "#363537 !important",
+  toggleBorder: "#FFF !important",
+  background: "#363537 !important",
+  border: "1px solid black",
+  borderRadius: "50%"
+};
+export const darkTheme = {
+  body: "#363537 !important",
+  text: "#FAFAFA !important",
+  toggleBorder: "#6B8096 !important",
+  border: "1px solid white",
+  borderRadius: "50%",
+  background: "#999 !important",
+};
